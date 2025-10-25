0171c8fcc3c21a7f576c760e06fe88b25da63341
649000 <649000@users.noreply.github.com>
2025-10-25 19:22:59 +0800

    Cleaned up dependencies

--- src/app/services/claim.service.ts
+++ src/app/services/claim.service.ts
@@ -22,36 +22,36 @@ export class ClaimService extends BaseApiService<Claim, number> {
 
   // Get claims by warranty ID
   getClaimsByWarrantyId(warrantyId: number, includeAuth: boolean = true): Observable<Claim[]> {
-    return this.http.get<Claim[]>(`${this.baseUrl}/${this.endpoint}/warranty/${warrantyId}`, { 
-      headers: this.getHeaders(includeAuth) 
+    return this.http.get<Claim[]>(`${this.baseUrl}/${this.endpoint}/warranty/${warrantyId}`, {
+      headers: this.getHeaders(includeAuth)
     });
   }
 
   // Get user's claims by status
   getClaimsByStatus(status: string, includeAuth: boolean = true): Observable<Claim[]> {
-    return this.http.get<Claim[]>(`${this.baseUrl}/${this.endpoint}/status/${status}`, { 
-      headers: this.getHeaders(includeAuth) 
+    return this.http.get<Claim[]>(`${this.baseUrl}/${this.endpoint}/status/${status}`, {
+      headers: this.getHeaders(includeAuth)
     });
   }
-  
+
   // Override base methods to support token handling
-  getAll(includeAuth: boolean = true): Observable<Claim[]> {
+  override getAll(includeAuth: boolean = true): Observable<Claim[]> {
     return super.getAll(includeAuth);
   }
-  
-  getById(id: number, includeAuth: boolean = true): Observable<Claim> {
+
+  override getById(id: number, includeAuth: boolean = true): Observable<Claim> {
     return super.getById(id, includeAuth);
   }
-  
-  create(claim: Omit<Claim, 'id'>, includeAuth: boolean = true): Observable<Claim> {
+
+  override create(claim: Omit<Claim, 'id'>, includeAuth: boolean = true): Observable<Claim> {
     return super.create(claim, includeAuth);
   }
-  
-  update(claim: Claim, includeAuth: boolean = true): Observable<Claim> {
+
+  override update(claim: Claim, includeAuth: boolean = true): Observable<Claim> {
     return super.update(claim, includeAuth);
   }
-  
-  delete(id: number, includeAuth: boolean = true): Observable<void> {
+
+  override delete(id: number, includeAuth: boolean = true): Observable<void> {
     return super.delete(id, includeAuth);
   }
 }
