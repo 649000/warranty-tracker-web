564b5ee6750debb8a5c3c6200cc7270cc3cd7aec
Nazri <649000@users.noreply.github.com>
2025-10-28 18:54:33 +0800

    New nextjs project

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { CompanyListComponent } from './components/company/company-list/company-list.component';
-
-export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'companies', component: CompanyListComponent },
-  { path: '**', redirectTo: '' }
-];
